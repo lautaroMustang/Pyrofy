d7fc09a032ecdac60ef287342c6ae0b0fda006e9
lautaroMustang <lautarogonzalez@outlook.com>
2019-10-10 12:36:28 -0300

    2do commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Pyrofy</title>
+  </head>
+  <body>
+    <h1>Prueba</h1>
+    <p>vamo</p>
+    <a href="/clients">Clientes</a>
+  </body>
+</html>
