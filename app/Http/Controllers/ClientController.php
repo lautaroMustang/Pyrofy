4c4d9cff2e50484aba3b1040f7df67d3abb2f13e
lautaroMustang <lautarogonzalez@outlook.com>
2019-10-16 11:54:22 -0300

    comienzo armado de clientes

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -13,7 +13,11 @@ class ClientController extends Controller
      */
     public function index()
     {
-        return view('clients');
+      $clients=\App\Client::all();
+      $variables=[
+        "clients"=>$clients,
+      ];
+        return view('clients',$clients);
     }
 
     /**
